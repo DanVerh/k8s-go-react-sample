ffff482472aa740885f5dd571976386e07741bb6
DanVerh <danverh29@gmail.com>
2023-09-08 23:48:35 +0300

    ingress fi  && frontend test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,34 @@
 import React, { useEffect, useState } from 'react';
 
 function App() {
-//  const [data, setData] = useState(null);
-//
-//  useEffect(() => {
-//    fetchData();
-//  }, []);
-//
-//  const fetchData = async () => {
-//    try {
-//      const response = await fetch('http://52.186.109.80//backend');
-//      const jsonData = await response.json();
-//      setData(jsonData);
-//    } catch (error) {
-//      console.error('Error fetching data:', error);
-//    }
-//  };
-//
-//  if (data === null) {
-//    return <div>Loading...</div>;
-//  }
+  const [data, setData] = useState(null);
 
+  useEffect(() => {
+    fetchData();
+  }, []);
 
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://localhost//backend');
+      const jsonData = await response.json();
+      setData(jsonData);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  if (data === null) {
+    return <div>Loading...</div>;
   // Render your component with the fetched data
-//  return (
-//    <ul>
-//    {data.map((item) => (
-//      <li>{item.hello}</li>
-//    ))}
-//  </ul>
-//  );
-//}
+  }
 
-return (
-  <h1>Hello</h1>
-);
+  return (
+    <ul>
+    {data.map((item) => (
+      <li>{item.hello}</li>
+    ))}
+  </ul>
+  );
 }
 
 export default App;
